b6c370aeb33eb69591fc57911aca8c6ba66a78d0
Anthony Gregis <anthony@thedevtron.com>
2020-07-20 17:04:12 -0500

    [JS/Readme] Add dom elements and an event listener

--- README.md
+++ README.md
@@ -24,6 +24,9 @@ let friends = ['Lauren', 'Cody', 'Jessica', 'Kyle', 'Nick']
 ```
 
 ##### Object
+
+When putting a function in an object using arrow functions will result in `this` being unusable as it doesn't bind to the parent object / function
+
 ```javascript
 let neuralink = {
     industry: "Neurotechnology",
@@ -60,3 +63,42 @@ const subtractNumbers = (num1, num2) => {
     return num1 - num2
 }
 ```
+
+## DOM
+
+##### Select a DOM Element
+```javascript
+const container = document.querySelector('.container')
+```
+
+##### Create a DOM Element
+```javascript
+const headerTwo = document.createElement('h2')
+headerTwo.textContent = "My First JS Review"
+```
+
+##### Append DOM Element to another
+```javascript
+container.appendChild(headerTwo)
+```
+
+##### Set a class in DOM Element
+```javascript
+headerTwo.classList.add('subtitle', 'header-two')
+headerTwo.setAttribute('class', 'header-two')
+```
+
+##### Remove a class in DOM Element
+```javascript
+headerTwo.classList.remove('header-two')
+```
+
+#### DOM Event Listeners
+
+##### DOM Event Listener (On clicking the DOM element)
+```javascript
+headerTwo.addEventListener('click', () => {
+    headerTwo.textContent = "Anthony"
+})
+```
+
--- js/app.js
+++ js/app.js
@@ -16,12 +16,12 @@ let neuralink = {
     }
 }
 
-console.log(age)
-console.log(isCool)
-console.log(friends)
+// console.log(age)
+// console.log(isCool)
+// console.log(friends)
 // console.log(neuralink)
-console.log(neuralink.ceo)
-console.log(neuralink.generalInfo())
+// console.log(neuralink.ceo)
+// console.log(neuralink.generalInfo())
 
 const printFriends = (array) => {
     array.forEach(friend => {
@@ -30,4 +30,43 @@ const printFriends = (array) => {
 }
 
 // printFriends(friends)
+
+// standard function
+function addNumbers(num1, num2) {
+    return num1 + num2
+}
+
+// function expression
+const multiplyNumbers = function(num1, num2) {
+    return num1 * num2
+}
+
+// arrow function
+const subtractNumbers = (num1, num2) => {
+    return num1 - num2
+}
+
+// DOM
+
+const container = document.querySelector('.container')
+console.log(container)
+
+// Create an element
+
+const headerTwo = document.createElement('h2')
+headerTwo.textContent = "My First JS Review"
+
+container.appendChild(headerTwo)
+
+// add a class to headerTwo
+headerTwo.classList.add('subtitle', 'header-two')
+headerTwo.setAttribute('class', 'header-two')
+
+//remove class from headerTwo
+headerTwo.classList.remove('header-two')
+console.log(headerTwo)
+
+headerTwo.addEventListener('click', () => {
+    headerTwo.textContent = "Anthony"
+})
 
